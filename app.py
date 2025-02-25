47952b863e092830eff1303abbe46d7ed24f40eb
Гунгер Кирилл <86ae85@gmail.com>
2025-02-25 18:05:43 +0700

    Первое приложение

--- app.py
+++ app.py
@@ -3,4 +3,20 @@ app = Flask (__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+            <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Гунгер Кирилл Андреевич, Лабораторная 1</title>
+        </head>
+        <body>
+            <header>
+                НГТУ, ФБ, Лабораторная работа 1
+            </header>
+            <h1>web-сервер на flask</h1>
+            <footer>
+                &copy; Гунгер Кирилл, ФБИ-23, 3 курс, 2025
+            </footer>
+        </body>
+    </html>
+    """
